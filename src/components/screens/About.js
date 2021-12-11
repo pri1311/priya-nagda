55574f4571ada2aad66db87ed3a2c7ec670cd743
pri1311 <nagdapriya13@gmail.com>
2021-12-11 21:50:30 +0530

    About page UI changes

--- src/components/screens/About.js
+++ src/components/screens/About.js
@@ -20,7 +20,8 @@ export default function About() {
                     </div>
                 </div>
                 <div className='imageWrapper'>
-                    <img className='aboutImage' src='/assets/images/undraw_voice_interface_eckp.svg'></img>
+                    {/* <img className='aboutImage' src='/assets/images/undraw_voice_interface_eckp.svg'></img> */}
+                    <img className='aboutImage' src='/assets/images/aboutme.png'></img>
                 </div>
             </div>
             <div className='bottomWrapper'>
@@ -34,8 +35,8 @@ export default function About() {
                 </div>
                 <div className='expWrapper'>
                     <h1 className='heading'>Experience</h1>
-                    <Experience company="iMumz- Pruoo Healthcare Technologies Pvt. Ltd." years="May - June 2021" summary="Redesigning the UI of profile screen, Feed, etc. in Flutter. Implemented Stories feature for the app, in a team. Bug fixes and code redesign to help make app more responsive."></Experience>
-                    <Experience company="Your Super Idea" years="May 2021" summary="Integrated firebase authentication. Built UI for app which uses location to find nearby vendors to deliver vegetables/fruits. Integrated google maps to display markers on map. Brainstorming Databasemodelrequired forthe app, considering both admin app and customer app usage."></Experience>
+                    <Experience company="iMumz- Pruoo Healthcare Technologies Pvt. Ltd." years="May - June 2021" summary={["Redesigning the UI of profile screen, Feed, etc. in Flutter.", " Implemented Stories feature for the app, in a team.", "Bug fixes and code redesign to help make app more responsive."]}></Experience>
+                    <Experience company="Your Super Idea" years="May 2021" summary={["Integrated firebase authentication.", "Built UI for app which uses location to find nearby vendors to deliver vegetables/fruits.", "Integrated google maps to display markers on map.", "Brainstorming Database model required for the app, considering both admin app and customer app usage."]}></Experience>
                 </div>
             </div>
         </div>
--- src/components/utils/Experience.js
+++ src/components/utils/Experience.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './Experience.css'
 
 export default function Experience(props) {
+    var summary = []
+    for (var i in props.summary) {
+        summary.push(<li key={i}>{props.summary[i]}</li>)
+    }
     return (
         <div className='experienceWrapper'>
             <h1 className='companyTitle'>{props.company}</h1>
             <h2 className='years'>{props.years}</h2>
-            <h2 className='summary'>{props.summary}</h2>
+            <h2 className='summary'>{summary}</h2>
         </div>
     )
 }
