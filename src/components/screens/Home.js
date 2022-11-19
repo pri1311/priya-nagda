639889c87384f739c17392f14e7af89eddb9eda9
Priya Nagda <nagdapriya13@gmail.com>
2022-11-19 15:20:07 +0530

    fix imports

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import home from '../../assets/home.png'
-import home_mobile from '../../assets/home_mobile.jpg'
 import '../styles/Home.css'
 import About from './About'
 import Contact from './Contact'
 import Projects from './Projects'
 
 export default function Home() {
-    var image = home;
-    if (window.innerWidth <= 440) {
-        image = home_mobile;
-    }
     return (
         <React.Fragment>
             <About />
