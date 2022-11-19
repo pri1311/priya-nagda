7fda0994940d13a217e9800dd33006b81d7c6275
Priya Nagda <nagdapriya13@gmail.com>
2022-11-19 15:20:42 +0530

    Updates

---
+++ precache-manifest.3af1b6a39b4da3a8ae2c70b4b9d9e38e.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "f817e9a2c1ee19576569",
+    "url": "/static/css/main.cd295030.chunk.css"
+  },
+  {
+    "revision": "f817e9a2c1ee19576569",
+    "url": "/static/js/main.181f1793.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "479e39c82ed6e3afcae9",
+    "url": "/static/js/2.0ac1e306.chunk.js"
+  },
+  {
+    "revision": "22ee40702ee60e32869a6e80d97cf37b",
+    "url": "/static/media/flask.22ee4070.svg"
+  },
+  {
+    "revision": "1109c64899990f3bb31842aa87a41d9a",
+    "url": "/static/media/flutter.1109c648.svg"
+  },
+  {
+    "revision": "d9237725db7f9cfd32560a8a93505d4b",
+    "url": "/static/media/javascript.d9237725.svg"
+  },
+  {
+    "revision": "96986fcae124e6ff6f2b24ad4f999242",
+    "url": "/static/media/nodejs.96986fca.svg"
+  },
+  {
+    "revision": "4336ae61013b18ea587d7544b1e2aafe",
+    "url": "/static/media/react.4336ae61.svg"
+  },
+  {
+    "revision": "57dfc3496da6a66b92d54a1fd106b53c",
+    "url": "/static/media/firebase.57dfc349.svg"
+  },
+  {
+    "revision": "6a3bfe5d1247b2d620debf98f4b6dc7a",
+    "url": "/static/media/mongodb.6a3bfe5d.svg"
+  },
+  {
+    "revision": "7cce028487f05a5e5e778e013a0b4ce5",
+    "url": "/static/media/mysql.7cce0284.svg"
+  },
+  {
+    "revision": "1cafcd5c70970c6a5712e3262ccce0af",
+    "url": "/static/media/bootstrap.1cafcd5c.svg"
+  },
+  {
+    "revision": "58c36dd6ab1d6f1761d06c023f570417",
+    "url": "/static/media/jquery.58c36dd6.svg"
+  },
+  {
+    "revision": "b6dd61c9a165e83bdfd07e9fb07d3889",
+    "url": "/static/media/css.b6dd61c9.svg"
+  },
+  {
+    "revision": "521bdc40ebd94461995a83048f9dde23",
+    "url": "/static/media/html.521bdc40.svg"
+  },
+  {
+    "revision": "49e2724a4ad3ae481747a67eabfdd167",
+    "url": "/static/media/crunch.49e2724a.png"
+  },
+  {
+    "revision": "c84292125d9d7d6c0f82004eda60698e",
+    "url": "/static/media/dyce&dyne.c8429212.png"
+  },
+  {
+    "revision": "527dedb80b395a6d67547d5d8761c437",
+    "url": "/static/media/earthling.527dedb8.png"
+  },
+  {
+    "revision": "894cd311626aa1b40d9b94295b794ac2",
+    "url": "/static/media/khetibadi.894cd311.png"
+  },
+  {
+    "revision": "cc27e5c926ffe7d15a5cb1428990b184",
+    "url": "/static/media/foco.cc27e5c9.png"
+  },
+  {
+    "revision": "a696831cca681a30ff530b6d1fb657c0",
+    "url": "/static/media/aboutme2.a696831c.png"
+  },
+  {
+    "revision": "6d25ae389f5f8ecf7f9dcb80076de116",
+    "url": "/static/media/c.6d25ae38.svg"
+  },
+  {
+    "revision": "c95c3642f8714cd2c01996eb17690632",
+    "url": "/static/media/c++.c95c3642.svg"
+  },
+  {
+    "revision": "9da33b058bf8980a407c2cd6840d0b91",
+    "url": "/static/media/dart.9da33b05.svg"
+  },
+  {
+    "revision": "37915013100e8c4756e490b1cc4695fe",
+    "url": "/static/media/kotlin.37915013.svg"
+  },
+  {
+    "revision": "13e7fc2bedc43afcce04e66cf5d4127f",
+    "url": "/static/media/php.13e7fc2b.png"
+  },
+  {
+    "revision": "9571895b144e28775fd433e6e51b073e",
+    "url": "/static/media/python.9571895b.svg"
+  },
+  {
+    "revision": "189aacfefda1c78e7825dad5528d5abe",
+    "url": "/static/media/typescript.189aacfe.svg"
+  },
+  {
+    "revision": "b4cebee8c22bf92d081f87e5272153bf",
+    "url": "/static/media/julia.b4cebee8.png"
+  },
+  {
+    "revision": "ab2bf628866bdff2d34d6d31e4ea0667",
+    "url": "/static/media/android.ab2bf628.svg"
+  },
+  {
+    "revision": "d51cfa4a35e802b60c57f166cdf288fd",
+    "url": "/static/media/git.d51cfa4a.svg"
+  },
+  {
+    "revision": "69b034d02c14ae459747c706e2dd47a0",
+    "url": "/static/media/json.69b034d0.svg"
+  },
+  {
+    "revision": "ba25f54326ded54c8aea9c5abbda36c4",
+    "url": "/static/media/android-studio.ba25f543.svg"
+  },
+  {
+    "revision": "c26a9e54e264ea0879b71ad6975b78fd",
+    "url": "/static/media/github.c26a9e54.svg"
+  },
+  {
+    "revision": "a8f96c5fe186814585782009101d4c94",
+    "url": "/static/media/intellij.a8f96c5f.svg"
+  },
+  {
+    "revision": "0ec516f71773d73edcdf57252016c869",
+    "url": "/static/media/vscode.0ec516f7.svg"
+  },
+  {
+    "revision": "8c6893f5f98107b933d9bd6db89bd615",
+    "url": "/static/media/cupy.8c6893f5.png"
+  },
+  {
+    "revision": "4d52d08b29f1f9031f23ac847c18f974",
+    "url": "/static/media/flux.4d52d08b.png"
+  },
+  {
+    "revision": "bcf7b3c74d00d6ed1601f4bc7bcc015d",
+    "url": "/static/media/numpy.bcf7b3c7.png"
+  },
+  {
+    "revision": "7bf8d5d293f52d7d627e4d8175ec952f",
+    "url": "/static/media/pandas.7bf8d5d2.png"
+  },
+  {
+    "revision": "f0f50003d0e332fe8232c3d685b6b221",
+    "url": "/static/media/sklearn.f0f50003.png"
+  },
+  {
+    "revision": "682b59a561d6f9a9243b981e3395c95a",
+    "url": "/static/media/pytorch.682b59a5.png"
+  },
+  {
+    "revision": "d09dc432a7bf2ced99b1c4a33d9498cd",
+    "url": "/index.html"
+  }
+];
